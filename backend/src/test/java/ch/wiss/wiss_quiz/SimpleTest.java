6151457d9afe946a5d67fb4cfeb3e01f17483f5a
Carl-Christopher Biebow <carl@luminocity.ch>
2026-03-02 07:54:32 +0100

    Create SimpleTest.java
    added first unit-test SimpleTest

---
+++ backend/src/test/java/ch/wiss/wiss_quiz/SimpleTest.java
@@ -0,0 +1,10 @@
+package ch.wiss.wiss_quiz;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SimpleTest {
+ @Test
+ void testSomething() {
+ assertEquals(4, 2 + 2);
+ }
+}
