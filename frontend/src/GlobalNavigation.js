ced8867aa89bbdff3c869d1aed4f9d220313c9c4
Vinci4600 <vincent.diergardt@gmail.com>
2026-04-14 11:37:10 +0200

    Radio Eingefügt

--- frontend/src/GlobalNavigation.js
+++ frontend/src/GlobalNavigation.js
@@ -1,34 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-class GlobalNavigation extends React.Component{
-    constructor(props){
-        super(props);
-    }
+class GlobalNavigation extends React.Component {
+  constructor(props) {
+    super(props);
 
-    render(){
-        return(
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/quiz">Quiz</Link>
-              </li>
-              <li>
-                <Link to="/rules">Rules</Link>
-              </li>
-              <li>
-                <Link to="/questionslist">Questions</Link>
-              </li>
-              <li>
-                <Link to="/aboutus">About Us</Link>
-              </li>
-            </ul>
-          </nav>
-        )
-    }
+    // 🎧 Radio Stream erstellen (WICHTIG: nur 1x!)
+    this.radio = new Audio(
+      "https://energyzuerich.ice.infomaniak.ch/energyzuerich-high.mp3"
+    );
+  }
+
+  playRadio = () => {
+    this.radio.load();
+    this.radio.play().catch(err => console.log(err));
+  };
+
+
+  stopRadio = () => {
+    this.radio.pause();
+    this.radio.currentTime = 0;
+  };
+
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/quiz">Quiz</Link></li>
+          <li><Link to="/rules">Rules</Link></li>
+          <li><Link to="/questionslist">Questions</Link></li>
+          <li><Link to="/aboutus">About Us</Link></li>
+        </ul>
+
+        <div className="radio-controls">
+          <button onClick={this.playRadio}>▶ Play Radio</button>
+          <button onClick={this.stopRadio}>⏹ Stop</button>
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default GlobalNavigation;
