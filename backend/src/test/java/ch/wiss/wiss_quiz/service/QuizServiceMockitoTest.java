7f7e34b6ebbb38b335349ce1da0309f487b7e216
LeoKuhnZH <leo.kuhn.zh@gmail.com>
2026-04-14 09:06:00 +0200

    got some QuizServiceMockitoTests

--- backend/src/test/java/ch/wiss/wiss_quiz/service/QuizServiceMockitoTest.java
+++ backend/src/test/java/ch/wiss/wiss_quiz/service/QuizServiceMockitoTest.java
@@ -1,20 +1,30 @@
 package ch.wiss.wiss_quiz.service;
 
-import ch.wiss.wiss_quiz.model.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import ch.wiss.wiss_quiz.model.Answer;
+import ch.wiss.wiss_quiz.model.Category;
+import ch.wiss.wiss_quiz.model.CategoryRepository;
+import ch.wiss.wiss_quiz.model.Question;
+import ch.wiss.wiss_quiz.model.QuestionRepository;
 
 @ExtendWith(MockitoExtension.class)
 class QuizServiceMockitoTest {
+
+    @InjectMocks
+    private QuizService service;
 
     @Mock
     private QuestionRepository questionRepository;
@@ -22,8 +32,7 @@ class QuizServiceMockitoTest {
     @Mock
     private CategoryRepository categoryRepository;
 
-    @InjectMocks
-    private QuizService service;
+
 
     // Station 4 – Service und Datenbank (Mockito)
     @Test
