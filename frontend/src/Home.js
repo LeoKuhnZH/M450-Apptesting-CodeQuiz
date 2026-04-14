ca6dee58bac146dc94bfbb70cda367105d01e6c3
Vinci4600 <vincent.diergardt@gmail.com>
2026-04-14 10:55:53 +0200

    Logo Anpassung

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import logo from "./components/Bilder/Wer wird Millionär Wiss Quiz Edition.png";
 
-class Home extends React.Component{
-    constructor(props){
+class Home extends React.Component {
+    constructor(props) {
         super(props);
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                <h4>Homepage</h4>
+                
+                <img
+                    src={logo}
+                    alt="Wer wird Millionär Wiss Quiz Edition"
+                    style={{ width: "500px", marginBottom: "20px" }}
+                />
+
                 <p>This is a one-page app which demonstrates the power of the React.js framework.</p>
             </div>
         )
